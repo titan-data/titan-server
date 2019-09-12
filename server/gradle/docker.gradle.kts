4e432e227b02a37d29cf0e3ccabe4be372308a43
Eric Schrock <Eric.Schrock@delphix.com>
2019-09-12 19:33:41 -0400

    test docker publish

--- server/gradle/docker.gradle.kts
+++ server/gradle/docker.gradle.kts
@@ -1,7 +1,12 @@
+val imageName = when(project.hasProperty("serverImageName")) {
+    true -> project.property("serverImageName")
+    false -> "titandata/titan"
+}
+
 var buildDockerServer = tasks.register<Exec>("buildDockerServer") {
     group = LifecycleBasePlugin.BUILD_GROUP
     description = "Build docker server image"
-    commandLine("docker", "build", "--no-cache", "-t", "delphix/titan:latest", "-f", "${project.projectDir}/docker/server.Dockerfile", "${project.projectDir}")
+    commandLine("docker", "build", "--no-cache", "-t", "$imageName:latest", "-f", "${project.projectDir}/docker/server.Dockerfile", "${project.projectDir}")
     mustRunAfter(tasks.named("shadowJar"))
 }
 
@@ -9,28 +14,28 @@ var buildDockerServer = tasks.register<Exec>("buildDockerServer") {
 tasks.register<Exec>("rebuildDockerServer") {
     group = LifecycleBasePlugin.BUILD_GROUP
     description = "Build docker server image"
-    commandLine("docker", "build", "-t", "delphix/titan:latest", "-f", "${project.projectDir}/docker/server.Dockerfile", "${project.projectDir}")
+    commandLine("docker", "build", "-t", "$imageName:latest", "-f", "${project.projectDir}/docker/server.Dockerfile", "${project.projectDir}")
     mustRunAfter(tasks.named("shadowJar"))
 }
 
 var tagDockerServer = tasks.register<Exec>("tagDockerServer") {
     group = LifecycleBasePlugin.BUILD_GROUP
     description = "Tag docker server image with current version"
-    commandLine("docker", "tag", "delphix/titan:latest", "delphix/titan:${project.version}")
+    commandLine("docker", "tag", "$imageName:latest", "$imageName:${project.version}")
     mustRunAfter(tasks.named("buildDockerServer"))
 }
 
 var tagLocalDockerServer = tasks.register<Exec>("tagLocalDockerServer") {
     group = LifecycleBasePlugin.BUILD_GROUP
     description = "Tag docker server image with current version"
-    commandLine("docker", "tag", "delphix/titan:latest", "titan:latest")
+    commandLine("docker", "tag", "$imageName:latest", "titan:latest")
     mustRunAfter(tasks.named("buildDockerServer"))
 }
 
 var buildSshServer = tasks.register<Exec>("buildSshTestServer") {
     group = LifecycleBasePlugin.BUILD_GROUP
     description = "Build SSH endtoend server image"
-    commandLine("docker", "build", "-t", "delphix/sshtest:latest", "-f", "${project.projectDir}/docker/sshtest.Dockerfile", "${project.projectDir}")
+    commandLine("docker", "build", "-t", "sshtest:latest", "-f", "${project.projectDir}/docker/sshtest.Dockerfile", "${project.projectDir}")
 }
 
 tasks.named("assemble").configure {
@@ -46,14 +51,14 @@ tasks.named("endtoendTest").configure {
 var publishDockerVersion = tasks.register<Exec>("publishDockerVersion") {
     group = "Publishing"
     description = "Publish versioned docker server image to docker hub"
-    commandLine("docker", "push", "delphix/titan:${project.version}")
+    commandLine("docker", "push", "$imageName:${project.version}")
     mustRunAfter(tasks.named("tagDockerServer"))
 }
 
 var publishDockerLatest = tasks.register<Exec>("publishDockerLatest") {
     group = "Publishing"
     description = "Publish latest docker server image to docker hub"
-    commandLine("docker", "push", "delphix/titan:latest")
+    commandLine("docker", "push", "$imageName:latest")
     mustRunAfter(tasks.named("publishDockerVersion"))
 }
 

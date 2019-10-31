81aa16e4876ecc6cd28eb6cea51f2531136e6e09
Eric Schrock <Eric.Schrock@delphix.com>
2019-10-31 09:27:20 -0400

    drop active set until we know we need it

--- server/src/main/kotlin/io/titandata/metadata/table/Repositories.kt
+++ server/src/main/kotlin/io/titandata/metadata/table/Repositories.kt
@@ -5,5 +5,4 @@ import org.jetbrains.exposed.sql.Table
 class Repositories : Table() {
     val name = varchar("name", 64).primaryKey()
     val metadata = varchar("metadata", 8192)
-    val activeSet = varchar("active_set", 32)
 }

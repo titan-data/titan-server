04fc43de5855b174014841f656a37106b6475e1c
Eric Schrock <eric.schrock@delphix.com>
2019-11-18 10:34:36 -0500

    move to stable remote providers (#81)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -39,12 +39,12 @@ dependencies {
     compile("com.zaxxer:HikariCP:3.4.1")
 
     // Remotes
-    compile("io.titandata:remote-sdk:0.0.11")
-    compile("io.titandata:nop-remote-server:0.0.5")
-    compile("io.titandata:ssh-remote-server:0.0.7")
-    compile("io.titandata:s3-remote-server:0.0.8")
-    compile("io.titandata:s3web-remote-server:0.0.8")
-    compile("io.titandata:delphix-remote-server:0.0.8")
+    compile("io.titandata:remote-sdk:0.1.0")
+    compile("io.titandata:nop-remote-server:0.1.0")
+    compile("io.titandata:ssh-remote-server:0.1.0")
+    compile("io.titandata:s3-remote-server:0.1.0")
+    compile("io.titandata:s3web-remote-server:0.1.0")
+    compile("io.titandata:delphix-remote-server:0.1.0")
 
     testCompile("com.h2database:h2:1.4.200")
     testCompile("io.ktor:ktor-server-test-host:$ktorVersion")
